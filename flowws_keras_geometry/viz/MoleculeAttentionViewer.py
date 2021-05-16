b0fb106f53a4eda64af1b7c71047122a843c1c41
Matthew Spellings <mspells@vectorinstitute.ai>
2021-05-16 14:19:32 -0400

    Improve flexibility of MoleculeAttentionViewer

--- flowws_keras_geometry/viz/MoleculeAttentionViewer.py
+++ flowws_keras_geometry/viz/MoleculeAttentionViewer.py
@@ -57,6 +57,28 @@ class MoleculeAttentionViewer(flowws.Stage):
             help='Frame (data index) to visualize'),
         Arg('particle', None, int, 0,
             help='Particle index to focus on'),
+        Arg('additive_rendering', '-a', bool, True,
+            help='If True, use additive rendering'),
+        Arg('fast_antialiasing', None, bool, False,
+            help='Use Fast Approximate Antialiasing (FXAA)'),
+        Arg('ambient_occlusion', None, bool, False,
+            help='Use Screen Space Ambient Occlusion (SSAO)'),
+        Arg('filter_value', None, float,
+            help='Filter bonds by minimum attention value'),
+        Arg('value_scale', None, float, 0.,
+            help='Factor to modify bond values by (after filter_value; in log-space)'),
+        Arg('diameter_scale', '-d', float, 1.,
+            help='Size scale for atomic diameters'),
+        Arg('clip_min', None, float, 0.,
+            help='Minimum value of attention weights (after scaling by value_scale) to clip to'),
+        Arg('clip_max', None, float, 1.,
+            help='Maximum value of attention weights (after scaling by value_scale) to clip to'),
+        Arg('cmap_s', None, float, 0., valid_values=flowws.Range(-3., 3., True),
+            help='Bond colormap s argument (0, 1, 2) -> (blue, red, green)'),
+        Arg('cmap_h', None, float, 1.2, valid_values=flowws.Range(0., 2., True),
+            help='Bond colormap h argument controlling saturation'),
+        Arg('cmap_r', None, float, 1., valid_values=flowws.Range(0., 8., True),
+            help='Bond colormap r argument controlling number of rotations to make'),
     ]
 
     def run(self, scope, storage):
@@ -78,6 +100,16 @@ class MoleculeAttentionViewer(flowws.Stage):
         type_color_map = [element_colors.get(i, (1., 0, 1, 1.)) for (i, j) in sorted(type_map.items())]
         self.type_color_map = np.array(type_color_map)
 
+        element_radii = {
+            1: .25,
+            6: .7,
+            7: .65,
+            8: .6,
+        }
+
+        type_radius_map = [element_radii.get(i, 1) for (i, j) in sorted(type_map.items())]
+        self.type_diameter_map = np.array(type_radius_map)*2
+
         self.arg_specifications['frame'].valid_values = flowws.Range(
             0, len(self.train_data[0]), (True, False))
         self.arg_specifications['particle'].valid_values = flowws.Range(
@@ -91,7 +123,14 @@ class MoleculeAttentionViewer(flowws.Stage):
         train_xs, train_ts = self.train_data
         i, j = self.arguments['frame'], self.arguments['particle']
 
-        features = dict(additive_rendering=True)
+        features = {}
+        if self.arguments.get('additive_rendering', True):
+            features['additive_rendering'] = True
+        if self.arguments.get('fast_antialiasing', False):
+            features['fxaa'] = True
+        if self.arguments.get('ambient_occlusion', False):
+            features['ssao'] = True
+
         prim = draw.Spheres()
         edges = draw.Lines(cap_mode=1)
         scene = draw.Scene([prim, edges], zoom=3.5, features=features)
@@ -107,21 +146,27 @@ class MoleculeAttentionViewer(flowws.Stage):
         end_points = []
         colors = []
         for (ii, jj) in itertools.product(range(len(xs)), range(len(xs))):
+            if self.arguments.get('filter_value', 0):
+                if attention[ii, jj] < self.arguments['filter_value']:
+                    continue
             start_points.append(xs[ii])
             end_points.append(xs[jj])
             colors.append(attention[ii, jj])
-        colors = np.array(colors)[:, 0]
-        colors *= np.sqrt(attention.shape[0])
-        colors = np.clip(colors, 0, 1)
-        colors = plato.cmap.cubehelix(colors)
+        colors = np.array(colors)[:, 0]*np.exp(self.arguments['value_scale'])
+        colors = np.clip(colors, self.arguments['clip_min'], self.arguments['clip_max'])
+        colors = plato.cmap.cubehelix(
+            colors, s=self.arguments['cmap_s'], h=self.arguments['cmap_h'],
+            r=self.arguments['cmap_r'])
         edges.start_points = start_points
         edges.end_points = end_points
         edges.widths = .125
         edges.colors = colors
 
+        types = np.argmax(train_ts[i, :len(xs)], axis=-1)
+
         prim.positions = xs
-        colors = self.type_color_map[np.argmax(train_ts[i, :len(xs)], axis=-1)]
+        colors = self.type_color_map[types]
         prim.colors = colors
-        prim.diameters = 1
+        prim.diameters = self.type_diameter_map[types]*self.arguments['diameter_scale']
 
         return scene
